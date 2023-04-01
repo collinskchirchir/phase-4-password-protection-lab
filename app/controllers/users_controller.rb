f220cf159553f0bbaf75e6106c1ed9d4205f8121
Collins Chirchir <collinskchirchir@gmail.com>
2023-04-01 13:09:26 +0300

    fix user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   before_action :authorize, only: :show
 
   def create
-    user = User.create!(user_params)
+    user = User.create(user_params)
     if user.valid?
       session[:user_id] = user.id
       render json: user, status: :created
     else
-      render json: {error: user.errors.full_messages}, status: :uprocessable_entity
+      render json: {error: user.errors.full_messages}, status: :unprocessable_entity
     end
 
-  end
+  end  
 
   def show
     user = User.find_by(id: session[:user_id])
