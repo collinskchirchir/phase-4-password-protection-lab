20e42f3ee96aa506f832e2461e50a7e016c80bc1
Collins Chirchir <collinskchirchir@gmail.com>
2023-04-01 11:48:18 +0300

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create]
+  # resources :users, only: [:create, :show]
+
+  # custom route for user signup
+  post '/signup', to: 'users#create'
+
+  # custom route for user authentication
+  get '/me', to: 'users#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
